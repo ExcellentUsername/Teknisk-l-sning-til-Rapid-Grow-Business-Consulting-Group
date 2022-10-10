a383c043a1cd85e68b0e05a0ba71352d1a11346e
ExcellentUsername <mikkelmoellerjensen04@gmail.com>
2022-10-10 09:35:42 +0200

    Added Array for translations

---
+++ TranslateArray.java
@@ -0,0 +1,8 @@
+public class TranslateArray {
+    String[][] TranslateArray = {{"målet","target"},{"mål","target"},{"nyt","bleeding-edge"},
+    {"ny","bleeding-edge"},{"god","excellent"},{"godt","excellent"},{"penge","profit"},
+    {"skov","økosystem"},{"mark","økosystem"},{"hav","økosystem"},{"sø","økosystem"},
+    {"bankerot","midlertidig nedtur"},{"mulighed","once-in-a-lifetime opportunity"},
+    {"lobbyisme","donationer til udvalgte politikere"},
+    {"strejke","nedlægning af nødvendig arbejdskraft"},{"fagforening","marxistisk bogklub"}};
+}
