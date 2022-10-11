76ae662abda113a79c1499d9f6c634878f0975ec
ExcellentUsername <mikkelmoellerjensen04@gmail.com>
2022-10-11 15:56:49 +0200

    Added close to functioning stringArray

--- App.java
+++ App.java
@@ -1,5 +1,31 @@
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.*; 
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) throws Exception{
+ 
+        File inputTekst = new File("tekst.txt"); //assigner filen tekst.txt
+        Scanner sc = new Scanner(inputTekst); 
+        ArrayList<String> arr = new ArrayList<>();
+
+        while(sc.hasNext()){ //Fylder arraylisten med individuelle strings fra teksten
+            String string = sc.next();
+
+            arr.add(string);
+         }
+
+            String[] stringArray = new String[arr.size()];
+
+            for(int i = 0; i <= arr.size(); i++){
+
+                stringArray[i] = arr.get(i);
+    
+            }
+
+            System.out.println(Arrays.toString(stringArray));
+
+
     }
 }
