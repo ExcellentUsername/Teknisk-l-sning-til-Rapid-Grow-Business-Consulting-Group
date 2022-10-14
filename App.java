5fe8ff50aa5e8d9492ebbfe31fb5ff3c5512eab4
PyromaniacalGoose <Mathiasjohnbeck@gmail.com>
2022-10-14 13:55:15 +0200

    u bad, u bald, u babby

--- App.java
+++ App.java
@@ -1,6 +1,7 @@
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Scanner;
+import javax.swing.JFrame;
 
 public class App {
     public static void main(String[] args) throws Exception {
